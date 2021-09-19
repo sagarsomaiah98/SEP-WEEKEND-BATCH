70c4c9d8b6d6cea0d2c7eb74753584e74b3bb334
sagar somaiah <somaiah1988@gmail.com>
2021-09-19 21:08:24 +0530

    java

---
+++ SampleProject/src/Hello_World.java
@@ -0,0 +1,13 @@
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Janbask");
+		System.out.println("Welcome to selenium training");
+		
+		String name = "John";
+		System.out.println(name);
+	}
+
+}
